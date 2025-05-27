e4802765ed98d390052adba564392ab1c5fcc93c
EshanAk-dev <eshanwijesinghe01@gmail.com>
2025-05-27 11:38:22 +0530

    Refactor app configuration and add SearchBar component

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -44,6 +44,8 @@ const _layout = () => {
           height: 52,
           position: "absolute",
           overflow: "hidden",
+          borderWidth: 1,
+          borderColor: "#0f0d23"
         }
       }}
     >
---
+++ components/SearchBar.tsx
@@ -0,0 +1,39 @@
+import { icons } from "@/constants/icons";
+import React from "react";
+import { Image, StyleSheet, TextInput, View } from "react-native";
+
+interface Props {
+  placeholder: string;
+  onPress?: () => void;
+} // Tells whats are the types of props
+
+const SearchBar = ({ placeholder, onPress }: Props) => {
+  return (
+    <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
+      <Image
+        source={icons.search}
+        className="size-5"
+        resizeMode="contain"
+        tintColor="#ab8bff"
+      />
+      <TextInput
+        onPress={onPress}
+        placeholder={placeholder}
+        value=""
+        onChangeText={() => {}}
+        placeholderTextColor="#ab85db"
+        style={styles.input}
+      />
+    </View>
+  );
+};
+
+export default SearchBar;
+
+const styles = StyleSheet.create({
+  input: {
+    flex: 1,
+    marginLeft: 12,
+    color: "#fff"
+  },
+});
