889b69b8b2eec4e8f322bc895d874da9867afe91
EshanAk-dev <eshanwijesinghe01@gmail.com>
2025-05-28 15:11:15 +0530

    Add Appwrite integration for search tracking and enhance search functionality

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,3 +1,4 @@
+import placeholderImage from "@/assets/images/placeholder.png";
 import { icons } from "@/constants/icons";
 import { Link } from "expo-router";
 import React from "react";
@@ -9,32 +10,35 @@ const MovieCard = ({
   title,
   vote_average,
   release_date,
-  original_language
+  original_language,
 }: Movie) => {
   return (
     <Link href={`/movies/${id}`} asChild>
       <TouchableOpacity className="w-[30%]">
         <Image
-          source={{
-            uri: poster_path
-              ? `https://image.tmdb.org/t/p/w500${poster_path}`
-              : "https://via.placeholder.com/600x400/1a1a1a/ffffff.png",
-          }}
+          source={
+            poster_path
+              ? { uri: `https://image.tmdb.org/t/p/w500${poster_path}` }
+              : placeholderImage
+          }
           className="w-full h-52 rounded-lg"
           resizeMode="cover"
         />
         {/* Display movie title with a maximum of one line */}
-        <Text className="text-sm font-bold text-white" numberOfLines={1}>{title}</Text> 
-        
+        <Text className="text-sm font-bold text-white" numberOfLines={1}>
+          {title}
+        </Text>
+
         <View className="flex-row items-center justify-start gap-x-1">
           <Image source={icons.star} className="size-4" />
           <Text className="text-xs text-white font-bold uppercase">
-            {(vote_average).toFixed(1)}
+            {vote_average.toFixed(1)}
           </Text>
         </View>
         <View className="flex-row items-center justify-between">
           <Text className="text-xs text-light-300 font-medium mt-1">
-            {release_date?.split("-")[0] || "N/A"} {/* Display year from release_date */}
+            {release_date?.split("-")[0] || "N/A"}{" "}
+            {/* Display year from release_date */}
           </Text>
           <Text className="text-xs text-light-300 font-medium uppercase">
             {original_language || "N/A"} {/* Display original language */}
